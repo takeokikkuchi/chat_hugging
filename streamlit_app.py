bbbf70541641727d7278b0d9ebbf7b35872ee00d
takeokikkuchi <96605483+takeokikkuchi@users.noreply.github.com>
2024-05-27 09:14:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from streamlit_chat import message
 from streamlit_extras.colored_header import colored_header
 from streamlit_extras.add_vertical_space import add_vertical_space
 from hugchat import hugchat
+from hugchat.login import Login
 
 st.set_page_config(page_title="HugChat - An LLM-powered Streamlit app")
 
